1796fb9aaa41ef6c3624c01cdf88de9a4905f883
yuri91 <yuri91@users.noreply.github.com>
2023-06-30 11:47:23 +0000

    deploy: 685d3d3120ffa1d047e6a8dd3d436fcce34ed538

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 + branch hinting (Draft 2022-02-24)',
-    LANGUAGE: 'None',
+    VERSION: '2.0 + branch hinting (Draft 2023-06-30)',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
