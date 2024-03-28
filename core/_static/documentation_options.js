337ff07a07cd322f436563e6eeb9e946fcd4f942
yuri91 <yuri91@users.noreply.github.com>
2024-03-28 13:57:56 +0000

    deploy: 425c7c776db2f5f9b54d04fefa36a2f71b7e0631

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 + branch hinting (Draft 2024-03-26)',
+    VERSION: '2.0 + branch hinting (Draft 2024-03-28)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
